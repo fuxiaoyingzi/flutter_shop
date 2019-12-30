5894eb4b1fa63ae70096b08a4ce753d483f4c6e2
fuyingying <fuyingying@91top.com>
2019-12-30 18:55:05 +0800

    1.分类 数据解析

--- lib/pages/config/service_url.dart
+++ lib/pages/config/service_url.dart
@@ -1,10 +1,13 @@
 const BASE_SERVICE_URL = "https://wxmini.baixingliangfan.cn/baixing/";
 const HOME_PAGE_CONTENT = "homePageContent";
 const HOME_PAGE_BELOW_CONTENT = "homePageBelowContent";
+const CATEGORY_CONTENT = "getCategory";
 const servicePath = {
   //商店首页信息
   HOME_PAGE_CONTENT: BASE_SERVICE_URL + "wxmini/homePageContent",
 
   //商店首页火爆商品
   HOME_PAGE_BELOW_CONTENT: BASE_SERVICE_URL + "wxmini/homePageBelowConten",
+
+  CATEGORY_CONTENT: BASE_SERVICE_URL + "wxmini/getCategory",
 };
--- lib/pages/tab/category_page.dart
+++ lib/pages/tab/category_page.dart
@@ -1,10 +1,35 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class CategoryPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/bean/CategoryBean.dart';
+import 'package:flutter_shop/pages/config/service_url.dart';
+import 'package:flutter_shop/pages/service/service_method.dart';
+
+class CategoryPage extends StatefulWidget {
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
+    _getCategoryData();
     return Container(
-      child: Text("分类"),
+      child: Text("分类页面"),
     );
+  }
+
+  void _getCategoryData() {
+    getRequestContent(CATEGORY_CONTENT).then((val) {
+      /* var clipboard = ClipboardData(text: val.toString());
+      Clipboard.setData(clipboard);
+      print(val.toString());*/
+
+      Map userMap = json.decode(val.toString());
+      var categoryBean = CategoryEntity.fromJson(userMap);
+      categoryBean.data.forEach((item) {
+        print(item.mallCategoryName);
+      });
+    });
   }
 }
